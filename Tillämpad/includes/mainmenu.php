f76b6d8c39ed266579e68784e574b434c675b6ad
Kiregnu <103045844+Kiregnu@users.noreply.github.com>
2022-05-03 09:27:14 +0200

    Add files via upload

---
+++ Tillämpad/includes/mainmenu.php
@@ -0,0 +1,8 @@
+<nav class="mainmenu">
+    <ul>
+        <li><a href="index.php">Hem</a></li>
+        <li><a href="omsidan.php">Om sidan</a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+        <li><a href="includes/logoutfunction.php">Logga ut</a></li>
+    </ul>
+</nav>
---
+++ Tillämpad/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styles.css" type="text/css">
+    </head>
+    <body>
+        <header id="mainheader">
+            <h1>Hejdå</h1>
+            <?php include("includes/mainmenu.php"); ?>
+        </header>
+        <div class="container">hg</div>
+    </body>
+</html>
